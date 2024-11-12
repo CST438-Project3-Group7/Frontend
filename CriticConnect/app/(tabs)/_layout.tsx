fa29311c76781f99547a30f20cff0ae0abd4d117
KrispyKris905 <celizarraraz@csumb.edu>
2024-11-12 04:11:38 -0800

    updated nav bar

--- CriticConnect/app/(tabs)/_layout.tsx
+++ CriticConnect/app/(tabs)/_layout.tsx
@@ -1,14 +1,17 @@
-import { Tabs } from "expo-router";
-export default ()=> {
-    return (
-        <Tabs>
-        <Tabs.Screen name="home" />
-        <Tabs.Screen name="login"/>
-        <Tabs.Screen name="signUp"/>
-        <Tabs.Screen name="cmovies" />
-        <Tabs.Screen name="cgames"/>
-        <Tabs.Screen name="cmusic"/>
-        <Tabs.Screen name="feed"/>
-        </Tabs>
-    );
+import React from 'react';
+import { Tabs } from 'expo-router';
+
+export default function RootLayout() {
+
+  return (
+    <Tabs>
+        <Tabs.Screen name="home" options={{ headerShown: false }} />
+        <Tabs.Screen name="login" options={{ headerShown: false }} />
+        <Tabs.Screen name="signup" options={{ headerShown: false }} />
+        <Tabs.Screen name="cmovies" options={{ headerShown: false }} />
+        <Tabs.Screen name="cgames" options={{ headerShown: false }} />
+        <Tabs.Screen name="cmusic" options={{ headerShown: false }} />
+        <Tabs.Screen name="feed" options={{ headerShown: false }} />
+    </Tabs>
+  );
 }
