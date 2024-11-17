cde03900c20df0f7060208888cbf5494ccbf12bd
KrispyKris905 <celizarraraz@csumb.edu>
2024-11-17 12:42:45 -0800

    attempt to add google oauth to both android and web. Web semi-functional.WIP

--- CriticConnect/app/(tabs)/signup.tsx
+++ CriticConnect/app/(tabs)/signup.tsx
@@ -1,39 +1,45 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity,TextInput, ScrollView } from 'react-native';
+import React, { useEffect,useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity,TextInput, ScrollView,Button } from 'react-native';
 import styles from '@/hooks/aMStyles';
 import { router } from "expo-router";
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
+const webClientId = '973280850643-799591b5eq02v6gcqsp59jneh22atura.apps.googleusercontent.com';
+
+WebBrowser.maybeCompleteAuthSession();
 
 const signUp = () => {
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: '340362220149-479mqi43ef9no1cmk88op3d08v10m0vp.apps.googleusercontent.com',
-    });
-  }, []);
+  const config={
+    webClientId,
+    androidClientId,
+  };
+  const [request,response,promptAsync] = Google.useAuthRequest(config);
+  const getUserProfile=async (token:any)=>{
+    if(!token) return;
+    try{
+      const response = await fetch('https://www.googleapis.com/userinfo/v2/me',{
+        headers: {Authrization:`Bearer ${token}`},
+      });
+      const user = await response.json();
+      console.log(user);
+    }catch(e){
+      console.error(e);
+    }
+  };
+  const handleToken=()=>{
+    if(response?.type === 'success'){
+      const {authentication} = response;
+      const token = authentication?.accessToken;
+      console.log("accessToken",token);
+      getUserProfile(token)
+    }
+  };
+    
 
-  async function onGoogleButtonPress() {
-    // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    // Get the users ID token
-    const signInResult = await GoogleSignin.signIn();
-  
-    // Try the new style of google-sign in result, from v13+ of that module
-    idToken = signInResult.data?.idToken;
-    if (!idToken) {
-      // if you are using older versions of google-signin, try old style result
-      idToken = signInResult.idToken;
-    }
-    if (!idToken) {
-      throw new Error('No ID token found');
-    }
-  
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(signInResult.data.token);
-  
-    // Sign-in the user with the credential
-    return auth().signInWithCredential(googleCredential);
-  }
+  useEffect(() => {
+    handleToken();
+  },[response]);
 
   return (
     <View>
@@ -76,6 +82,15 @@ const signUp = () => {
         <TouchableOpacity style={styles.link} onPress={() => router.push('/login')}>
           <Text style={styles.linkText}>Already have an account? Login.</Text>
         </TouchableOpacity>
+        <View style={styles.link}>
+          <Text>or</Text>
+        </View>
+        <View>
+          <TouchableOpacity onPress={()=> promptAsync()}>
+            <Text>Sign in with Google</Text>
+          </TouchableOpacity>
+          <Text>{JSON.stringify(response)}</Text>
+        </View>
       </View>
       </View>
   );
