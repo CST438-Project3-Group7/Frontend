63ecf4d8a27e10d80f49da35122119024a581b32
Jashan Rahal <jashanrahal@Jashans-MacBook-Pro.local>
2024-12-07 10:16:03 -0800

    renamed file to match layout

--- CriticConnect/app/(tabs)/_layout.tsx
+++ CriticConnect/app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
         <Tabs.Screen name="Home" options={{ headerShown: false }} />
         <Tabs.Screen name="Login" options={{ headerShown: false }} />
         <Tabs.Screen name="Signup" options={{ headerShown: false }} />
-        <Tabs.Screen name="Select Favorites" options={{ headerShown: false }} />
+        <Tabs.Screen name="Favorites" options={{ headerShown: false }} />
         <Tabs.Screen name="Feed" options={{ headerShown: false }} />
         <Tabs.Screen name="Profile" options={{ headerShown: false }} />
         <Tabs.Screen name="post" options={{ headerShown: false }} />
